8bb5627cc369e410c788bb66c60be4eb4caaf3c7
Ashrafansar1 <ashraf27.mrj@gmail.com>
2025-10-04 15:18:17 +0530

    Added testing1 class

---
+++ src/test/java/AutomationTesting/AutomationTesting/testing1.java
@@ -0,0 +1,10 @@
+package AutomationTesting.AutomationTesting;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class testing1 {
+	driver = new ChromeDriver();
+    driver.manage().window().maximize();
+    driver.get(URL);
+}
+}
